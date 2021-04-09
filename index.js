8fb292fb519fca4e6b842e989b54e1e743bf9c65
matukyury <matuky@tut.by>
2021-04-09 16:38:58 +0300

    firat day

--- index.js
+++ index.js
@@ -1,9 +1,76 @@
-function sayHello (name) {
-    if (name = Mark)
-    return true;
-let message = 'Hi, ' + name;
+function sayHello(name) {
+    console.log("Hello, " + name + "!");{
+    if (name="Mark")
+    console.log("Hi, " + name + "!");}
+
 
 }
+  
+sayHello("Oleg");
+sayHello("Viktor");
+sayHello("Mark");
 
-console.log ( sayHello );
+const n = [3];
+const m = [4];
+console.log (( Math.pow(m, 2) ) + (Math.pow(n, 2)));
+
+console.log (Math.hypot(n, m));
+
+
+const a = (2)
+const b = (5)
+
+console.log (Math.min(a, b))
+
+
+const c = (3)
+const d = (-1)
+
+console.log (Math.min(c, d))
+
+const e = (1)
+const f = (1)
+
+console.log (Math.min(e, f))
+
+
+const g = (4)
+    if (g % 2 == 0) 
+        
+console.log(g)
+
+
+const chars1 = "Hello";
+
+console.log(chars1.slice(1, - 1));
+
+const chars2 = "A";
+
+console.log(chars2.slice(1, - 1));
+
+
+
+
+const fn1 = (1, 2, 3, "4", "5", "6")
+
+console.log( String(1), String(2), String(3), Number("4"), Number("5"), Number("6") );
+
+
+
+
+const firstArray = [1, 2, 3];
+const secondArray = [4, 5, 6];
+const thirdArray = [...firstArray, ...secondArray];
+
+console.log(thirdArray);
+
+
+
+const str = 'Привет мир!';
+
+console.log(str.substr(0, 6) + "...");
+// expected output: "oz"
+
+
+
 
